820c4357886714d6dd1577c4e99b485e5b1d21ae
David Eisinger <david.eisinger@viget.com>
2009-03-14 16:02:15 -0400

    Basic DB test

--- app/models/db.rb
+++ app/models/db.rb
@@ -1,2 +1,3 @@
 class Db < ActiveRecord::Base
+  validates_presence_of :name, :occupation
 end
--- test/unit/db_test.rb
+++ test/unit/db_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class DbTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "A DB" do
+    should_validate_presence_of :name, :occupation
   end
 end
