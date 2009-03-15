7bf922908aba39f5aad0e817c05f583690079863
David Eisinger <david.eisinger@viget.com>
2009-03-15 17:06:31 -0400

    DB accepts avatar_id

--- app/models/db.rb
+++ app/models/db.rb
@@ -2,12 +2,24 @@ class Db < ActiveRecord::Base
   validates_presence_of :name, :occupation
   has_one :avatar
 
+  after_save :set_avatar
+
   def avatar=(attrs = {})
     self.build_avatar(attrs)
+  end
+
+  def avatar_id=(num)
+    @avatar = Avatar.find(num)
   end
 
   def bagfactor
     sleep(2)
     rand(50) / 10.0
+  end
+
+  private
+
+  def set_avatar
+    @avatar.update_attribute(:db_id, self.id) if @avatar
   end
 end
--- test/unit/db_test.rb
+++ test/unit/db_test.rb
@@ -9,5 +9,11 @@ class DbTest < ActiveSupport::TestCase
       db = Db.new
       assert_equal Float, db.bagfactor.class
     end
+
+    should "accept an avatar_id" do
+      avatar = Avatar.create(:image => File.open(RAILS_ROOT + "/test/criss_angel.jpg"))
+      db = Db.create(:name => "Criss Angel", :occupation => "Illusionist", :avatar_id => avatar.id)
+      assert_equal avatar, db.reload.avatar
+    end
   end
 end
