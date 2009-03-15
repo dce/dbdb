bd5af83e28f35ef3bcff3dba84c9bbee356f3688
David Eisinger <david.eisinger@viget.com>
2009-03-14 21:02:42 -0400

    Added AJAX version #3 (loading profiles in order after page loads)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -31,5 +31,33 @@ $(document).ready(function() {
       return false;
     });
   });
+
+  jQuery.fn.loadContentInOrder = function() {
+    link = this;
+    if (this.attr("href") != null) {
+      $.ajax({url: this.attr("href"),
+        success: function(src) {
+          display = link.parents("dt").next("dd").replaceWith(src).css("display");
+          link.parents("dt").next("dd").css("display", display)
+            .next("dt").find("a").loadContentInOrder();
+        }
+      });
+    }
+  };
+
+  $("a#version-3").click(function() {
+    $(".db-list a").each(function() {
+      $(this).parents("dt").after("<dd class=\"spinner\"></dd>").next("dd").hide();
+    });
+
+    $(".db-list a:first").each(function() {
+      $(this).loadContentInOrder();
+    });
+
+    $(".db-list a").click(function() {
+      $(this).parents("dt").next("dd").toggle();
+      return false;
+    });
+  });
 });
 
