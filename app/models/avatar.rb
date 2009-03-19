931b5b0fcbf337fd3908fa04577ef94162f01f82
David Eisinger <david.eisinger@viget.com>
2009-03-19 10:24:36 -0700

    Increasing upload time

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,4 +1,12 @@
 class Avatar < ActiveRecord::Base
   has_one :db
   has_attached_file :image, :styles => { :profile => "150x150#", :thumb => "50x50#" }
+
+  before_save :sit_and_wait
+
+  private
+
+  def sit_and_wait
+    sleep(5) unless RAILS_ENV == "test"
+  end
 end
